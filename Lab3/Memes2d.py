c6f09f6bfb36d8f0d00893bd8dfdf6cc954369a3
Jagolen <jakob.golen@gmail.com>
2022-05-10 15:37:22 +0200

    blabla

--- Lab3/Memes2d.py
+++ Lab3/Memes2d.py
@@ -5,7 +5,15 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import random
+import igraph
 
 G = nx.grid_2d_graph(4,4, [False, False])
 nx.draw(G)
+plt.show()
+
+
+G2 = nx.read_pajek("Roget.net")
+G3 = nx.Graph(G2)
+
+nx.draw(G3)
 plt.show()
